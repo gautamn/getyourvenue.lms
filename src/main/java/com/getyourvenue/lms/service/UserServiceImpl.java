51ad06ba9df66a0f1d453c599f02694649055bbd
Nitin Gautam <Nitin.Gautam@NOD-BF0-LO-C024.INDIATIMES.CO.IN>
2012-12-15 22:03:24 +0530

    MD5 hash implementation

--- src/main/java/com/getyourvenue/lms/service/UserServiceImpl.java
+++ src/main/java/com/getyourvenue/lms/service/UserServiceImpl.java
@@ -1,16 +1,15 @@
 package com.getyourvenue.lms.service;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.bouncycastle.crypto.digests.MD5Digest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.getyourvenue.lms.dao.IUserDao;
 import com.getyourvenue.lms.model.User;
+import com.getyourvenue.lms.utils.LMSUtils;
 
 @Service
 public class UserServiceImpl implements IUserService{
@@ -35,23 +34,7 @@ public class UserServiceImpl implements IUserService{
 	@Transactional
 	public boolean isValidUser(String emailid, String password) {
 		
-		/*byte[] data = password.getBytes(); 
-		MD5Digest md5;
-		
-		try {
-			md5 = MD5Digest.class.newInstance();
-			md5.update(data,0,data.length); 
-	        int length = md5.getDigestSize();
-	        byte[] md5ByteArr = new byte[length];
-	        md5.doFinal(md5ByteArr, 0);
-			password=new String(md5ByteArr);
-			
-		} catch (Exception e) {
-			logger.error("Error occured while generating MD5 password!!!", e);
-		}
-		
-		logger.debug("Passed changed to:"+password);*/
-		
+		password=LMSUtils.getMD5Hash(password);
 		return userHibDaoImpl.isValidUser(emailid, password);
 	}
 }
